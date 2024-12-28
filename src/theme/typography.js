c3265418c91b14a39b351a1a0589afb9f4fe6ee3
denizbinay <deniz@bombig.net>
2024-12-28 22:13:05 +0100

    remove css variables build step and use json variables in tailwind.config.js directly

---
+++ src/theme/typography.js
@@ -0,0 +1,16 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+    theme: {
+        extend: {
+            fontSize: {
+                'global-text-xs': ['undefined', {
+                    lineHeight: 'undefined',
+                    letterSpacing: 'undefined'
+                }]
+            }
+        }
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+import tokens from './tokens/tokens.json' assert { type: 'json' };
+
 export default {
     darkMode: 'class',
     content: [
         './src/**/*.{js,jsx,ts,tsx}',
     ],
     theme: {
+        fontSize: {
+            'xs': [
+                tokens.global['text-xs'].value.fontSize,
+                {
+                    lineHeight: tokens.global['text-xs'].value.lineHeight,
+                    letterSpacing: tokens.global['text-xs'].value.letterSpacing,
+                }
+            ]
+        },
         extend: {
             colors: {
-                brand: 'rgb(var(--global-brand))',
-                secondary: 'rgb(var(--global-secondary))',
-                black: 'rgb(var(--global-black))',
-                white: 'rgb(var(--global-white))',
+                brand: tokens.global.brand.value,
+                secondary: tokens.global.secondary.value,
+                black: tokens.global.black.value,
+                white: tokens.global.white.value,
             }
-        },
+        }
     },
     plugins: [],
 }
